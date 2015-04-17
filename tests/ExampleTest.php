ff8b680f7412c6aea742fdbfa792bfbf1c700c1e
Luciano Jr <luciano@lucianojr.com.br>
2015-04-16 23:28:25 -0300

    Fix the route evento test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,7 +9,7 @@ class ExampleTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
-		$response = $this->call('GET', '/');
+		$response = $this->call('GET', '/evento/');
 
 		$this->assertEquals(200, $response->getStatusCode());
 	}
